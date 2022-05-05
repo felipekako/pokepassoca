e7c0927d036f896862f0144d49fbe1ef5ab570ef
felipekako <m>
2022-05-05 11:31:26 -0300

    criando header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logoball from "./image/ultra-ball.png";
+import styled from 'styled-components';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      <p>p</p> <Logoball src={logoball}/>  <p>kemon</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+const Logoball = styled.img`
+ width: 100px;
+ height:100px;
+`
